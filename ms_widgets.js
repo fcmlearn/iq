5c85fdbc523334e41e9660f2d357fe784b940497
dicuyozet <51477117+dicuyozet@users.noreply.github.com>
2019-06-16 03:10:48 +0530

    cmd res

--- ms_widgets.js
+++ ms_widgets.js
@@ -336,14 +336,136 @@ class MSWidgets extends ActivityHandler {
             default:
                 await context.sendActivity({
                     attachments: [
-                        this.adaptiveCard(context),
-                        this.animationCard(context),
-                        this.audioCard(context),
-                        this.heroCard(context),
-                        this.receiptCard(context),
-                        this.signInCard(context),
-                        this.thumbnailCard(context),
-                        this.videoCard(context)
+                        CardFactory.adaptiveCard({
+                            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                            "type": "AdaptiveCard",
+                            "version": "1.0",
+                            "body": [
+                                {
+                                "type": "Image",
+                                "url": "http://adaptivecards.io/content/cats/1.png"
+                                }
+                            ]
+                        }),
+                        CardFactory.animationCard(
+                            'Frittata',
+                            [
+                                'https://media.giphy.com/media/TL060X7wVIfOU/giphy.gif'
+                            ],
+                            [{
+                                type: 'openUrl',
+                                title: 'Lean More',
+                                value: 'https://channel9.msdn.com/Events/Imagine-Cup/World-Finals-2018/2018-Imagine-Cup-World-Championship-Intro'
+                            }],
+                            {
+                                subtitle: 'Frittata is an egg-based Italian dish similar to an omelette or crustless quiche or scrambled eggs, enriched with additional ingredients such as meats, cheeses or vegetables. The word frittata is Italian and roughly translates to "fried."'
+                            }
+                        ),
+                        CardFactory.audioCard(
+                            'I am your father',
+                            ['https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_yourfather.wav'],
+                            CardFactory.actions([
+                                {
+                                    type: 'openUrl',
+                                    title: 'Read more',
+                                    value: 'https://en.wikipedia.org/wiki/The_Empire_Strikes_Back'
+                                }
+                            ]),
+                            {
+                                subtitle: 'Star Wars: Episode V - The Empire Strikes Back',
+                                text: 'The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.',
+                                image: 'https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg'
+                            }
+                        ),
+                        CardFactory.heroCard(
+                            'Food Wars!',
+                            'For those out of the loop (or who just aren\'t interest in anime) Shokugeki no Soma, or Food Wars!, is one of the hottest manga adaptations on television (and Netflix) at the moment. While most people consider it just another NSFW anime (there are a lot of boobs, not gonna lie) it actually has a pretty solid plot and some pretty amazing foodporn. Whether you\'re a fan of the show or just like experimenting in the kitchen you should definitely try these unbelievable dishes from Food Wars!',
+                            ['https://blog.williams-sonoma.com/wp-content/uploads/2016/12/jan-15-Shoyu-Ramen-with-Pork-Belly.jpg'],
+                            [
+                                {
+                                    type: ActionTypes.OpenUrl,
+                                    title: 'Gotcha Pork Roast',
+                                    value: 'https://media.giphy.com/media/WDuKqiuUPETYI/giphy.gif'
+                                },
+                                {
+                                    type: ActionTypes.OpenUrl,
+                                    title: 'Souffle Omelette',
+                                    value: 'https://media.giphy.com/media/W3jb1UC5Zt4pq/giphy.gif'
+                                },
+                                {
+                                    type: ActionTypes.OpenUrl,
+                                    title: 'Sumire Karaage Roll',
+                                    value: 'https://media.giphy.com/media/HSSJphsM5t744/giphy.gif'
+                                }
+                            ]
+                        ),
+                        CardFactory.receiptCard({
+                            title: 'John Doe',
+                            facts: [
+                                {
+                                    key: 'Order Number',
+                                    value: '1234'
+                                },
+                                {
+                                    key: 'Payment Method',
+                                    value: 'VISA 5555-****'
+                                }
+                            ],
+                            items: [
+                                {
+                                    title: 'Data Transfer',
+                                    price: '$38.45',
+                                    quantity: 368,
+                                    image: { url: 'https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png' }
+                                },
+                                {
+                                    title: 'App Service',
+                                    price: '$45.00',
+                                    quantity: 720,
+                                    image: { url: 'https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png' }
+                                }
+                            ],
+                            tax: '$7.50',
+                            total: '$90.95',
+                            buttons: CardFactory.actions([
+                                {
+                                    type: 'openUrl',
+                                    title: 'More information',
+                                    value: 'https://azure.microsoft.com/en-us/pricing/details/bot-service/'
+                                }
+                            ])
+                        }),
+                        CardFactory.signinCard(
+                            'BotFramework Sign in Card',
+                            'https://login.microsoftonline.com',
+                            'Sign in'
+                        ),
+                        CardFactory.thumbnailCard(
+                            'BotFramework Thumbnail Card',
+                            [{ url: 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg' }],
+                            [{
+                                type: 'openUrl',
+                                title: 'Get started',
+                                value: 'https://docs.microsoft.com/en-us/azure/bot-service/'
+                            }],
+                            {
+                                subtitle: 'Your bots — wherever your users are talking.',
+                                text: 'Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.'
+                            }
+                        ),
+                        CardFactory.videoCard(
+                            '2018 Imagine Cup World Championship Intro',
+                            [{ url: 'https://sec.ch9.ms/ch9/783d/d57287a5-185f-4df9-aa08-fcab699a783d/IC18WorldChampionshipIntro2.mp4' }],
+                            [{
+                                type: 'openUrl',
+                                title: 'Lean More',
+                                value: 'https://channel9.msdn.com/Events/Imagine-Cup/World-Finals-2018/2018-Imagine-Cup-World-Championship-Intro'
+                            }],
+                            {
+                                subtitle: 'by Microsoft',
+                                text: 'Microsoft\'s Imagine Cup has empowered student developers around the world to create and innovate on the world stage for the past 16 years. These innovations will shape how we live, work and play.'
+                            }
+                        )
                     ],
                     attachmentLayout: AttachmentLayoutTypes.Carousel
                 });
@@ -352,29 +474,30 @@ class MSWidgets extends ActivityHandler {
     
     // widget methods
     async heroCard(context) {
-        const card = CardFactory.heroCard(
-            'Food Wars!',
-            'For those out of the loop (or who just aren\'t interest in anime) Shokugeki no Soma, or Food Wars!, is one of the hottest manga adaptations on television (and Netflix) at the moment. While most people consider it just another NSFW anime (there are a lot of boobs, not gonna lie) it actually has a pretty solid plot and some pretty amazing foodporn. Whether you\'re a fan of the show or just like experimenting in the kitchen you should definitely try these unbelievable dishes from Food Wars!',
-            ['https://blog.williams-sonoma.com/wp-content/uploads/2016/12/jan-15-Shoyu-Ramen-with-Pork-Belly.jpg'],
-            [
-                {
-                    type: ActionTypes.OpenUrl,
-                    title: 'Gotcha Pork Roast',
-                    value: 'https://media.giphy.com/media/WDuKqiuUPETYI/giphy.gif'
-                },
-                {
-                    type: ActionTypes.OpenUrl,
-                    title: 'Souffle Omelette',
-                    value: 'https://media.giphy.com/media/W3jb1UC5Zt4pq/giphy.gif'
-                },
-                {
-                    type: ActionTypes.OpenUrl,
-                    title: 'Sumire Karaage Roll',
-                    value: 'https://media.giphy.com/media/HSSJphsM5t744/giphy.gif'
-                }
-            ]
-        );
-        await context.sendActivity({ attachments: [card] });
+        await context.sendActivity({ attachments: [
+            CardFactory.heroCard(
+                'Food Wars!',
+                'For those out of the loop (or who just aren\'t interest in anime) Shokugeki no Soma, or Food Wars!, is one of the hottest manga adaptations on television (and Netflix) at the moment. While most people consider it just another NSFW anime (there are a lot of boobs, not gonna lie) it actually has a pretty solid plot and some pretty amazing foodporn. Whether you\'re a fan of the show or just like experimenting in the kitchen you should definitely try these unbelievable dishes from Food Wars!',
+                ['https://blog.williams-sonoma.com/wp-content/uploads/2016/12/jan-15-Shoyu-Ramen-with-Pork-Belly.jpg'],
+                [
+                    {
+                        type: ActionTypes.OpenUrl,
+                        title: 'Gotcha Pork Roast',
+                        value: 'https://media.giphy.com/media/WDuKqiuUPETYI/giphy.gif'
+                    },
+                    {
+                        type: ActionTypes.OpenUrl,
+                        title: 'Souffle Omelette',
+                        value: 'https://media.giphy.com/media/W3jb1UC5Zt4pq/giphy.gif'
+                    },
+                    {
+                        type: ActionTypes.OpenUrl,
+                        title: 'Sumire Karaage Roll',
+                        value: 'https://media.giphy.com/media/HSSJphsM5t744/giphy.gif'
+                    }
+                ]
+            )
+        ] });
     }
 
     async cardPrompt(context){
@@ -402,9 +525,13 @@ class MSWidgets extends ActivityHandler {
                 CardFactory.animationCard(
                     'Frittata',
                     [
-                        { url: 'https://media.giphy.com/media/TL060X7wVIfOU/giphy.gif' }
+                     'https://media.giphy.com/media/TL060X7wVIfOU/giphy.gif'
                     ],
-                    [],
+                    [{
+                        type: 'openUrl',
+                        title: 'Lean More',
+                        value: 'https://channel9.msdn.com/Events/Imagine-Cup/World-Finals-2018/2018-Imagine-Cup-World-Championship-Intro'
+                    }],
                     {
                         subtitle: 'Frittata is an egg-based Italian dish similar to an omelette or crustless quiche or scrambled eggs, enriched with additional ingredients such as meats, cheeses or vegetables. The word frittata is Italian and roughly translates to "fried."'
                     }
@@ -551,6 +678,11 @@ class MSWidgets extends ActivityHandler {
                 type: ActionTypes.PostBack,
                 title: 'Eggs Benedict',
                 value: 'Eggs Benedict'
+            },
+            {
+                type: 'openUrl',
+                title: 'Get started',
+                value: 'https://docs.microsoft.com/en-us/azure/bot-service/'
             }
         ];
         await content.sendActivity(
